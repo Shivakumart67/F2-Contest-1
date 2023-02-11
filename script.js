db027a94137c04a16434fd713566ef1f3ed61fa8
Shivakumart67 <shivakumart67@gmail.com>
2023-02-11 13:09:28 +0530

    Completed

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-/** @format */
+/* @format */
 
 let arr = [
   { id: 1, name: "john", age: "18", marks: 80 },
@@ -8,20 +8,41 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  let passedStudents = arr.map(student => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
+  
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(student => {
+    if (student.marks > 50) {
+      console.log(student);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = {id:4, name:"susan", age:20, marks:45};
+  arr.push(newStudent);
+  console.log(newStudent);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let newarray = arr.filter(student => student.marks >= 50);
+  console.log(newarray);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newStudents = [{id:5, name:"tim", age:19, marks:60},
+                       {id:6, name:"jessica", age:21, marks:70},
+                       {id:7, name:"mark", age:18, marks:80}];
+  let concatenatedArray = arr.concat(newStudents);
+  console.log(concatenatedArray);
 }
